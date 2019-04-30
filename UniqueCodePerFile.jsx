e33e50ad9b48c3540f59b392870063668cd19571
Hebert Marchesi <hello@bertz.co>
2019-04-29 21:40:09 -0300

    File

---
+++ UniqueCodePerFile.jsx
@@ -0,0 +1,72 @@
+main();
+function main() { 
+	mySetup();
+}
+
+function mySetup() {
+	//Set the open file
+	var myDocument = app.activeDocument;
+	//Set the master spread
+	var myMasterSpread = myDocument.masterSpreads.item("A-Master");
+	try{
+		myMasterSpread.name;
+	}
+	catch(myError){
+		//If doesn't exist, create the master spread
+		myMasterSpread = myDocument.masterSpreads.add({name:"A-Master"});
+	}
+
+	try{
+		myColorB = myDocument.colors.item("CodeColor");
+		//If the color does not exist, trying to get its name will generate an error.
+		myName = myColorB.name;
+	}
+	catch (myError){
+		//The color style did not exist, so create it.
+		myColorB = myDocument.colors.add({name:"CodeColor", model:ColorModel.process, colorValue:[0, 0, 0, 1]}); }
+
+		//Create a dialog
+		var dlg = new Window('dialog', "Code Generator");  
+	      
+	    dlg.panel = dlg.add('panel', undefined, "Insert the total");  
+	    dlg.panel.alignChildren = "fill";  
+	    dlg.panel_text1 = dlg.panel.add('edittext', undefined, "");  
+	    dlg.panel = dlg.add('button', undefined, 'OK');
+	    dlg.panel = dlg.add('button', undefined, 'Cancel');
+	  	
+	    dlg.show();
+
+		var CurrentFile = 1;
+		var TotalFiles = dlg.panel_text1.text;
+		var NumberSerie = "001";
+		while(CurrentFile <= TotalFiles) {
+			for (var x = 0;x <= 979;x++) {
+				if (CurrentFile < 10) {
+					NumberSerie = NumberSerie + "00" + CurrentFile + " ";
+				}
+				else {
+					NumberSerie = NumberSerie + "0" + CurrentFile + " ";
+				}
+			}
+			var codeArea = myMasterSpread.textFrames.add({geometricBounds:[0,0,297, 420], contents: "" + NumberSerie});
+			var codeText = codeArea.parentStory.paragraphs.item(0)
+			codeText.appliedFont = app.fonts.item("Avenir Next");
+			codeText.pointSize = 20;
+			codeText.fontStyle = "Demi Bold"
+			codeText.fillColor = myColorB;
+			mySnippet(CurrentFile);
+			CurrentFile++;
+			NumberSerie = "";
+			codeText.remove();
+		}
+
+	
+
+		}
+
+	function mySnippet(number){
+	var myDocument = app.activeDocument;
+	var myNewName = myDocument.name.replace(".indd",""); 
+	var finalName = myNewName + "_" + number
+	app.activeDocument.exportFile(ExportFormat.pdfType, File(Folder.desktop + "/" + finalName + ".pdf"), false);
+}
