cb31e10df7c106e231038886c9a712e473098f5a
Bertz <bertz@MacBertz-Pro.local>
2020-11-14 20:30:32 -0300

    Update & fix code and format a README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 <p align="center"><img src="https://i.ibb.co/fYCMgVP/Unique-Code-Per-File-Header.jpg"></p>
 
-Export a set of .PDF files with unique code per file, like a security watermark.<br>
+<p align="center">Export a set of .PDF files with unique code per file, like a security watermark.</p><br>
+
+---
+## Overview
+This script saves a sequence of files in .PDF format with a unique number code per file in all pages of the file.
+
+In default mode, the PDF Preset is *Smallest File Size*<br>
+
+If you want to change that, you need to edit the line #91 in the file with the name of your preset.<br>
+
+```javascript
+var myPDFExportPreset = app.pdfExportPresets.item('[Smallest File Size]');
+```
+
+Feel free to open an issue or do a pull request.
--- UniqueCodePerFile.jsx
+++ UniqueCodePerFile.jsx
@@ -1,4 +1,5 @@
 main();
+
 function main() { 
 	mySetup();
 }
@@ -6,29 +7,34 @@ function main() {
 function mySetup() {
 	//Set the open file
 	var myDocument = app.activeDocument;
+
 	//Set the master spread
 	var myMasterSpread = myDocument.masterSpreads.item("A-Master");
-	try{
+
+
+
+	try {
 		myMasterSpread.name;
 	}
-	catch(myError){
+	catch(myError) {
 		//If doesn't exist, create the master spread
 		myMasterSpread = myDocument.masterSpreads.add({name:"A-Master"});
 	}
 
-	try{
+	try {
 		myColorB = myDocument.colors.item("CodeColor");
 		//If the color does not exist, trying to get its name will generate an error.
 		myName = myColorB.name;
 	}
-	catch (myError){
+	catch (myError) {
 		//The color style did not exist, so create it.
-		myColorB = myDocument.colors.add({name:"CodeColor", model:ColorModel.process, colorValue:[0, 0, 0, 1]}); }
+		myColorB = myDocument.colors.add({name:"CodeColor", model:ColorModel.process, colorValue:[0, 0, 0, 1]}); 
+	}
 
 		//Create a dialog
-		var dlg = new Window('dialog', "Code Generator");  
+		var dlg = new Window('dialog', "Unique Code Generator");  
 	      
-	    dlg.panel = dlg.add('panel', undefined, "Insert the total");  
+	    dlg.panel = dlg.add('panel', undefined, "Insert the total of files");  
 	    dlg.panel.alignChildren = "fill";  
 	    dlg.panel_text1 = dlg.panel.add('edittext', undefined, "");  
 	    dlg.panel = dlg.add('button', undefined, 'OK');
@@ -39,6 +45,7 @@ function mySetup() {
 		var CurrentFile = 1;
 		var TotalFiles = dlg.panel_text1.text;
 		var NumberSerie = "001";
+
 		while(CurrentFile <= TotalFiles) {
 			for (var x = 0;x <= 979;x++) {
 				if (CurrentFile < 10) {
@@ -48,25 +55,40 @@ function mySetup() {
 					NumberSerie = NumberSerie + "0" + CurrentFile + " ";
 				}
 			}
+
 			var codeArea = myMasterSpread.textFrames.add({geometricBounds:[0,0,297, 420], contents: "" + NumberSerie});
-			var codeText = codeArea.parentStory.paragraphs.item(0)
+			var codeText = codeArea.parentStory.paragraphs.item(0);
+
 			codeText.appliedFont = app.fonts.item("Avenir Next");
 			codeText.pointSize = 20;
 			codeText.fontStyle = "Demi Bold"
 			codeText.fillColor = myColorB;
+
 			mySnippet(CurrentFile);
+
 			CurrentFile++;
 			NumberSerie = "";
 			codeText.remove();
-		}
-
-	
 
 		}
 
-	function mySnippet(number){
+		
+
+		}
+
+	function mySnippet(number) {
+
+	//Get the file name
 	var myDocument = app.activeDocument;
-	var myNewName = myDocument.name.replace(".indd",""); 
+
+	//Clear the .indd in the name of file
+	var myNewName = myDocument.name.replace(".indd","");
+
+	//Insert the number in the file name
 	var finalName = myNewName + "_" + number
-	app.activeDocument.exportFile(ExportFormat.pdfType, File(Folder.desktop + "/" + finalName + ".pdf"), false);
+
+	//Set the smallest file size
+	var myPDFExportPreset = app.pdfExportPresets.item('[Smallest File Size]');
+
+	app.activeDocument.exportFile(ExportFormat.pdfType, File(Folder.desktop + "/" + finalName + ".pdf"), false, myPDFExportPreset);
 }
